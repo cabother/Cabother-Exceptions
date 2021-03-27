4004a88066355dd5321b34d88c96ebc5bf32a1d7
Renato Fernandes Soares <renato.f.soares@hotmail.com>
2021-03-26 22:49:45 -0300

    feat: added gateway timeout exception

---
+++ src/Cabother.Exceptions/Requests/GatewayTimeoutException.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Runtime.Serialization;
+
+namespace Cabother.Exceptions.Requests
+{
+    /// <summary>
+    /// Exceção de Timeout do Gateway
+    /// </summary>
+    [Serializable]
+    public class GatewayTimeoutException : Exception
+    {
+        /// <summary>
+        /// Cria uma nova instância de <see cref="GatewayTimeoutException"/>
+        /// </summary>
+        /// <param name="info">Informações do objeto sobre a exceção que está sendo lançada</param>
+        /// <param name="context">Informação de local onde a exception será lançada, seu contexto de origem/destino</param>
+        private GatewayTimeoutException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+
+        }
+
+        /// <summary>
+        /// Cria uma nova instância de <see cref="GatewayTimeoutException"/>
+        /// </summary>
+        /// <param name="message">Mensagem descritiva da exceção</param>
+        public GatewayTimeoutException(string message) : base(message)
+        {
+        }
+
+    }
+}
