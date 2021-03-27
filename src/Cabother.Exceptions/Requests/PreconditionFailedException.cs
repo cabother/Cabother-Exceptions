a6b506b8073dea0f70812bd455dc3424a6f7626e
Renato Fernandes Soares <renato.f.soares@hotmail.com>
2021-03-26 22:50:19 -0300

    feat: added precondition failed exception

---
+++ src/Cabother.Exceptions/Requests/PreconditionFailedException.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Runtime.Serialization;
+
+namespace Cabother.Exceptions.Requests
+{
+    /// <summary>
+    /// Exceção de falha na pré-condição nas entidades
+    /// </summary>
+    [Serializable]
+    public class PreconditionFailedException : Exception
+    {
+        /// <summary>
+        /// Cria uma nova instância de <see cref="PreconditionFailedException"/>
+        /// </summary>
+        /// <param name="info">Informações do objeto sobre a exceção que está sendo lançada</param>
+        /// <param name="context">Informação de local onde a exception será lançada, seu contexto de origem/destino</param>
+        private PreconditionFailedException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+
+        }
+
+        /// <summary>
+        /// Cria uma nova instância de <see cref="PreconditionFailedException"/>
+        /// </summary>
+        /// <param name="message">Mensagem descritiva da exceção</param>
+        public PreconditionFailedException(string message) : base(message)
+        {
+        }
+
+    }
+}
